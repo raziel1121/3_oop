c41d8a876f2d1f19109fe79eace91575b6422a5c
Jeff Cohen <cohen.jeff@gmail.com>
2017-06-01 11:13:53 -0500

    At the lab

--- demos/1_person.rb
+++ demos/1_person.rb
@@ -1,13 +1,47 @@
-# Code goes here
+# # Code goes here
+require_relative "2_logging"
+
 class Person
 
-end
+  include Logging
 
+  attr_accessor :last_name
+  attr_accessor :first_name
+  attr_accessor :sport
+
+  def initialize(favorite_sport = "hockey")
+    @sport = favorite_sport
+    write("Someone likes #{@sport}")
+  end
+
+  def to_s
+    return "#{full_name} likes #{sport}"
+  end
+
+  def full_name
+    write("Someone called full_name")
+    return "#{self.first_name} #{self.last_name}"
+  end
+end
+#
+# jeff = Person.new
+# jeff.first_name = "Jeff"
+# puts jeff.first_name
+#
+#
 
 class Employee < Person
 
-end
+  attr_accessor :salary
 
-class Manager < Employee
+  def self.titles
+    return ["Worker", "Manager", "Clown"]
+  end
+
+  def do_work
+    write("I did some work today!")
+  end
 
 end
+obj = Employee.new
+obj.do_work
--- demos/2_logging.rb
+++ demos/2_logging.rb
@@ -1,1 +1,10 @@
 # Logging Module
+module Logging
+
+  def write(msg)
+    file = open("logging.txt", "a")
+    file.puts msg
+    file.close
+  end
+
+end
