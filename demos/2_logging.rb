7ce125faa57ee2fce2325b6c8e19ba17ef2fed01
Jeff Cohen <cohen.jeff@gmail.com>
2017-06-01 11:20:42 -0500

    Fixed included hook method

--- demos/2_logging.rb
+++ demos/2_logging.rb
@@ -1,6 +1,10 @@
 # Logging Module
 module Logging
 
+  def self.included(klass)
+    puts "(Logging has been activated by #{klass.name})"
+  end
+
   def write(msg)
     file = open("logging.txt", "a")
     file.puts msg
